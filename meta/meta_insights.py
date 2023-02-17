89345ea7ca45fb204c48e5f17de6caa65229c549
Migolina <114832031+Migolina@users.noreply.github.com>
2023-02-17 17:19:07 +0300

    Update meta_insights.py

--- meta/meta_insights.py
+++ meta/meta_insights.py
@@ -5,6 +5,7 @@ import json
 from mysql_orm.models import AdSeries,Ads,AgeGender,Country
 from mysql_orm import session
 from sqlalchemy import update
+from calendar import monthrange
 
 today = date.today()
 
@@ -272,7 +273,142 @@ class MetaInsights:
 
 
 
+        def update_monthly_ad_series(self):
+        dt = today - timedelta(days=3)
+        year_and_month = monthrange(dt.year,dt.month)
+        month_to_use = year_and_month[1]
+        for i in range(1, month_to_use):
+            day_decrease = timedelta(days=i)
+            date_to_use = today - timedelta(days=3)
+            date_to_use = date_to_use - day_decrease
+            strdate = str(date_to_use.year) + '-' + str(date_to_use.month) + '-' + str(date_to_use.day)
 
+            params = {
+                "level": self.level,
+                'time_range': {'since': strdate, 'until': strdate},
+            }
+
+            monthly_ad_series = self.account.get_insights(params=params, fields=self.fields)
+            monthly_ad_series = [dict(item) for item in monthly_ad_series]
+            actions = {}
+            for ad in monthly_ad_series:
+                if ad.get("actions", None):
+                    for _type in ad["actions"]:
+                        actions[_type["action_type"]] = _type["value"]
+
+                session.query(AdSeries).filter(AdSeries.ad_id == ad["ad_id"],
+                                                AdSeries.date == ad["date_start"]) \
+                    .update({"impressions": ad["impressions"],
+                             "clicks": ad["clicks"],
+                             "total_spend": ad["spend"],
+                             "video_view": actions.get("video_view", 0),
+                             "comment": actions.get("comment", 0),
+                             "link_click": actions.get("link_click", 0),
+                             "post_reaction": actions.get("post_reaction", 0),
+                             "landing_page_view": actions.get("landing_page_view", 0),
+                             "post_engagement": actions.get("post_engagement", 0),
+                             "leadgen_grouped": actions.get("leadgen_grouped", 0),
+                             "lead": actions.get("lead", 0),
+                             "page_engagement": actions.get("page_engagement", 0),
+                             "onsite_conversion_post_save": actions.get("onsite_conversion_post_save", 0),
+                             "onsite_conversion_lead_grouped": actions.get("onsite_conversion_lead_grouped", 0),
+                             "offsite_conversion_fb_pixel_lead": actions.get("offsite_conversion_fb_pixel_lead", 0),
+                             "frequency": ad["frequency"]})
+
+        session.commit()
+
+    def update_monthly_age_and_gender(self):
+        dt = today - timedelta(days=3)
+        year_and_month = monthrange(dt.year, dt.month)
+        month_to_use = year_and_month[1]
+        for i in range(1, month_to_use):
+            day_decrease = timedelta(days=i)
+            date_to_use = today   - timedelta(days=3)
+            date_to_use = date_to_use - day_decrease
+            strdate = str(date_to_use.year) + '-' + str(date_to_use.month) + '-' + str(date_to_use.day)
+
+            params = {
+                "level": self.level,
+                'time_range': {'since': strdate, 'until': strdate},
+                "breakdowns": ["age", "gender"]
+            }
+
+            monthly_age_and_gender = self.account.get_insights(params=params, fields=self.fields)
+            monthly_age_and_gender = [dict(item) for item in monthly_age_and_gender]
+            actions = {}
+            for ad in monthly_age_and_gender:
+                if ad.get("actions", None):
+                    for _type in ad["actions"]:
+                        actions[_type["action_type"]] = _type["value"]
+
+                session.query(AgeGender).filter(AgeGender.ad_id == ad["ad_id"],
+                                                AgeGender.age == ad["age"],
+                                                AgeGender.gender == ad["gender"],
+                                                AgeGender.date == ad["date_start"]) \
+                    .update({"impressions": ad["impressions"],
+                             "clicks": ad["clicks"],
+                             "total_spend": ad["spend"],
+                             "video_view": actions.get("video_view", 0),
+                             "comment": actions.get("comment", 0),
+                             "link_click": actions.get("link_click", 0),
+                             "post_reaction": actions.get("post_reaction", 0),
+                             "landing_page_view": actions.get("landing_page_view", 0),
+                             "post_engagement": actions.get("post_engagement", 0),
+                             "leadgen_grouped": actions.get("leadgen_grouped", 0),
+                             "lead": actions.get("lead", 0),
+                             "page_engagement": actions.get("page_engagement", 0),
+                             "onsite_conversion_post_save": actions.get("onsite_conversion_post_save", 0),
+                             "onsite_conversion_lead_grouped": actions.get("onsite_conversion_lead_grouped", 0),
+                             "offsite_conversion_fb_pixel_lead": actions.get("offsite_conversion_fb_pixel_lead", 0),
+                             "frequency": ad["frequency"]})
+
+        session.commit()
+
+    def update_monthly_country(self):
+        dt = today - timedelta(days=3)
+        year_and_month = monthrange(dt.year, dt.month)
+        month_to_use = year_and_month[1]
+        for i in range(1, month_to_use):
+            day_decrease = timedelta(days=i)
+            date_to_use = today  - timedelta(days=3)
+            date_to_use = date_to_use - day_decrease
+            strdate = str(date_to_use.year) + '-' + str(date_to_use.month) + '-' + str(date_to_use.day)
+
+            params = {
+                "level": self.level,
+                'time_range': {'since': strdate, 'until': strdate},
+                "breakdowns": ["country"]
+            }
+
+            monthly_country = self.account.get_insights(params=params, fields=self.fields)
+            monthly_country = [dict(item) for item in monthly_country]
+            actions = {}
+            for ad in monthly_country:
+                if ad.get("actions", None):
+                    for _type in ad["actions"]:
+                        actions[_type["action_type"]] = _type["value"]
+
+                session.query(Country).filter(Country.ad_id == ad["ad_id"],
+                                              Country.country == ad["country"],
+                                              Country.date == ad["date_start"]) \
+                    .update({"impressions": ad["impressions"],
+                             "clicks": ad["clicks"],
+                             "total_spend": ad["spend"],
+                             "video_view": actions.get("video_view", 0),
+                             "comment": actions.get("comment", 0),
+                             "link_click": actions.get("link_click", 0),
+                             "post_reaction": actions.get("post_reaction", 0),
+                             "landing_page_view": actions.get("landing_page_view", 0),
+                             "post_engagement": actions.get("post_engagement", 0),
+                             "leadgen_grouped": actions.get("leadgen_grouped", 0),
+                             "lead": actions.get("lead", 0),
+                             "page_engagement": actions.get("page_engagement", 0),
+                             "onsite_conversion_post_save": actions.get("onsite_conversion_post_save", 0),
+                             "onsite_conversion_lead_grouped": actions.get("onsite_conversion_lead_grouped", 0),
+                             "offsite_conversion_fb_pixel_lead": actions.get("offsite_conversion_fb_pixel_lead", 0),
+                             "frequency": ad["frequency"]})
+
+        session.commit()
 
 
 
